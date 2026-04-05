e553f66c53310f4758e349217794a55cf51590e4
tan <tancodes@proton.me>
2026-04-05 15:47:19 -0600

    fix vercel rewrite destination and use runtime require for serverless

--- backend/api/serverless.ts
+++ backend/api/serverless.ts
@@ -1,1 +1,11 @@
-export { default } from '../dist/vercel';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+let handler: (req: VercelRequest, res: VercelResponse) => Promise<any>;
+
+export default async function (req: VercelRequest, res: VercelResponse) {
+  if (!handler) {
+    const mod = require('../dist/vercel');
+    handler = mod.default;
+  }
+  return handler(req, res);
+}
